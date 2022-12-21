3fdfff96de9398c80957d029e5b05b58a23d4f42
wstepniewski <wiktor.stepniewski@gmail.com>
2022-12-21 12:56:16 +0100

    construct

--- cwiczenie 7/cmake-build-debug/cmake_install.cmake
+++ cwiczenie 7/cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /Users/wiktorstepniewski/Desktop/Podstawy Programowania/cwiczenie 7
+# Install script for directory: /Users/wiktorstepniewski/Desktop/SynologyDrive/Podstawy Programowania/cwiczenie 7
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/Users/wiktorstepniewski/Desktop/Podstawy Programowania/cwiczenie 7/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/Users/wiktorstepniewski/Desktop/SynologyDrive/Podstawy Programowania/cwiczenie 7/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
