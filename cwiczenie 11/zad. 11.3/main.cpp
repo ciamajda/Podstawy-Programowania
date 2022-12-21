73b1016a8156c262cda32ba72441e2caaa7015df
wstepniewski <wiktor.stepniewski@gmail.com>
2022-12-21 18:08:49 +0100

    ver.1.1.

--- cwiczenie 11/zad. 11.3/main.cpp
+++ cwiczenie 11/zad. 11.3/main.cpp
@@ -1,31 +1,63 @@
 #include <iostream>
 #include <bitset>
+#include <fstream>
+#include <string>
+
 using namespace std;
-char *encrypt(char zdanie[], char key[]){
-    char encryptedArr[]={};
-    int j=0;
-    for(int i=0; i<12; i++) {
-        if(j>3 ? j=0 : NULL) {
-            encryptedArr[i] = (zdanie[i] ^ key[j]);
-            j++;
+const int N = 12; // Liczba znakow do zaszyfrowqania+1(znak konca)
+void encrypt(char tab[], char klucz[])
+{
+    char enrypted[N] = {};
+    int tmp = 0;
+    for (int i = 0; i < N - 1; i++)
+    {
+        if (tmp != 4 ?: tmp = 0)
+        {
+            enrypted[i] = tab[i] ^ klucz[tmp];
+            tmp++;
         }
     }
-    return encryptedArr;
+    ofstream encryptedtxt;
+    encryptedtxt.open("encrypted.txt", ios::trunc);
+    if (encryptedtxt.is_open())
+    {
+        for (int i = 0; i < N; i++)
+        {
+            encryptedtxt << enrypted[i];
+        }
+    }
+    encryptedtxt.close();
 }
 
-int main() {
-    char tab[12]={"Ala ma kota"}, key[4]={'a', 'b', 'c', 'd'}, encryptedArr[12]={};
+int main()
+{
+    string test;
+    char zdanie[N] = {}, klucz[4] = {};
+    ifstream input, key;
 
-    char *a=encrypt(tab, key);
-    cout<<encrypt(tab, key);
-    for(int i=0; i<11; i++){
-        cout<<tab[i];
+    input.open("nowy.txt", ios::binary);
+    if (input.is_open())
+    {
+        input.getline(&zdanie[0], N, '.');
     }
-    cout<<endl;
-//    cout<<*a<<endl;
-    for(int i=0; i<11; i++){
-        cout<<encryptedArr[i];
+    else
+        cout << "Plik nie istnieje.";
+    input.close();
+
+    key.open("klucz.txt");
+    if (key.is_open())
+    {
+        key.getline(&klucz[0], 5, '.');
     }
-    cout<<encrypt(tab, key);
+    else
+        cout << "Plik nie istnieje.";
+    key.close();
+
+    encrypt(zdanie, klucz);
+    for (int i = 0; i < N; i++)
+    {
+        cout << zdanie[i];
+    }
+
     return 0;
 }
